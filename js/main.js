e0414b9a2b2494dae4785224ae067f3661c7fa1d
juant21 <juan.esteban.tobias@gmail.com>
2024-02-24 16:34:09 -0500

    se creo el array para los productos
    se creo el array para los productos en venta y se agredo el aparto de pantalones en el index

---
+++ js/main.js
@@ -0,0 +1,242 @@
+[
+    {
+        "id": "abrigo-01",
+        "titulo": "Abrigo Masculino 1",
+        "imagen": "./img/Habrigo1.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+            },
+        "precio": 120000
+    },
+    {
+        "id": "abrigo-02",
+        "titulo": "Abrigo Masculino 2",
+        "imagen": "./img/Habrigo2.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+            },
+        "precio": 120000
+    },
+    {
+        "id": "abrigo-03",
+        "titulo": "Abrigo Masculino 3",
+        "imagen": "./img/Habrigo3.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+            },
+        "precio": 120000
+    },
+    {
+        "id": "abrigo-04",
+        "titulo": "Abrigo Femenino 1 ",
+        "imagen": "./img/Mabrigo1.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+            },
+        "precio": 120000
+    },
+    {
+        "id": "abrigo-05",
+        "titulo": "Abrigo Femenino 2",
+        "imagen": "./img/Mabrigo2.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+            },
+        "precio": 120000
+    },
+    {
+        "id": "abrigo-06",
+        "titulo": "Abrigo Femenino 3",
+        "imagen": "./img/Mabrigo3.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+            },
+        "precio": 120000
+    },
+    {
+        "id": "pantalon-01",
+        "titulo": "Pantalón Masculino  1",
+        "imagen": "./img/pantalon1.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 100000
+    },
+    {
+        "id": "pantalon-02",
+        "titulo": "Pantalón Masculino 2 ",
+        "imagen": "./img/pantalon2.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 100000
+    },
+    {
+        "id": "pantalon-03",
+        "titulo": "Pantalón Masculino 3",
+        "imagen": "./img/pantalon3.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 100000
+    },
+    {
+        "id": "pantalon-04",
+        "titulo": "Pantalón Femenino 1",
+        "imagen": "./img/Mpantalones1.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 100000
+    },
+    {
+        "id": "pantalon-05",
+        "titulo": "Pantalón Femenino 2",
+        "imagen": "./img/Mpantalones2.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 100000
+    },
+    {
+        "id": "pantalon-06",
+        "titulo": "Pantalón Femenino 3",
+        "imagen": "./img/Mpantalones3.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"bermuda-01",
+        "titulo":"Bermuda Masculina 1",
+        "imagen":"./imagenes/Bermudah1.jpg",
+        "categoria": {
+            "nombre": "Bermudas",
+            "id": "Bermudas"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"bermuda-02",
+        "titulo":"Bermuda Masculina 2",
+        "imagen":"./imagenes/bermudah2.jpg",
+        "categoria": {
+            "nombre": "Bermudas",
+            "id": "Bermudas"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"bermuda-03",
+        "titulo":"Bermuda Masculina 3",
+        "imagen":"./imagenes/bermudah3.jpg",
+        "categoria": {
+            "nombre": "Bermudas",
+            "id": "Bermudas"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"bermuda-04",
+        "titulo":"Bermuda Femenina 1",
+        "imagen":"./imagenes/BermudaM1.jpg",
+        "categoria": {
+            "nombre": "Bermudas",
+            "id": "Bermudas"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"bermuda-05",
+        "titulo":"Bermuda Femenina 2",
+        "imagen":"./imagenes/bermudaM2.jpg",
+        "categoria": {
+            "nombre": "Bermudas",
+            "id": "Bermudas"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"bermuda-06",
+        "titulo":"Bermuda Femenina 3",
+        "imagen":"./imagenes/bermudaM3.jpg",
+        "categoria": {
+            "nombre": "Bermudas",
+            "id": "Bermudas"
+        },
+        "precio": 100000
+    },
+    {
+        "id":"camiseta-01",
+        "titulo":"Camiseta Masculina 1",
+        "imagen":"./imagenes/camiseta1.jpg",
+        "categoria": {
+            "nombre": "camiseta",
+            "id": "camiseta"
+        },
+        "precio": 90000
+    },
+    {
+        "id":"camiseta-02",
+        "titulo":"Camiseta Masculina 2",
+        "imagen":"./imagenes/camiseta2.jpg",
+        "categoria": {
+            "nombre": "camiseta",
+            "id": "camiseta"
+        },
+        "precio": 90000
+    },
+    {
+        "id":"camiseta-03",
+        "titulo":"Camiseta Masculina 3",
+        "imagen":"./imagenes/camiseta3.jpg",
+        "categoria": {
+            "nombre": "camiseta",
+            "id": "camiseta"
+        },
+        "precio": 90000
+    },
+    {
+        "id":"camiseta-04",
+        "titulo":"Camiseta Femenina 4",
+        "imagen":"./imagenes/camiseta4.jpg",
+        "categoria": {
+            "nombre": "camiseta",
+            "id": "camiseta"
+        },
+        "precio": 90000
+    },
+    {
+        "id":"camiseta-05",
+        "titulo":"Camiseta Femenina 5",
+        "imagen":"./imagenes/camiseta5.jpg",
+        "categoria": {
+            "nombre": "camiseta",
+            "id": "camiseta"
+        },
+        "precio": 90000
+    },
+    {
+        "id":"camiseta-06",
+        "titulo":"Camiseta Femenina 6",
+        "imagen":"./imagenes/camiseta6.jpg",
+        "categoria": {
+            "nombre": "camiseta",
+            "id": "camiseta"
+        },
+        "precio": 90000
+    },
+]
