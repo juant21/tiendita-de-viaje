02154ea9a9a0ed405a96423b31107327d3f76190
juant21 <juan.esteban.tobias@gmail.com>
2024-02-25 14:05:08 -0500

    mejor organizacion

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
  const productos =  [
     {
         id: "abrigo-01",
-        titulo: "Abrigo Masculino 1",
-        imagen: "./imaganes/Habrigo1.jpg",
+        titulo: "Abrigo Masculino",
+        imagen: "./imagenes/abrigo1.jpg",
         categoria: {
             nombre: "Abrigos",
             id: "abrigos"
@@ -11,8 +11,8 @@
     },
     {
         id: "abrigo-02",
-        titulo: "Abrigo Masculino 2",
-        imagen: "./imagenes/Habrigo2.jpg",
+        titulo: "Abrigo Masculino",
+        imagen: "./imagenes/abrigo2.jpg",
         categoria: {
             nombre: "Abrigos",
             id: "abrigos"
@@ -21,38 +21,28 @@
     },
     {
         id: "abrigo-03",
-        titulo: "Abrigo Masculino 3",
-        imagen: "./imagenes/Habrigo3.jpg",
+        titulo: "Abrigo Masculino",
+        imagen: "./imagenes/abrigo3.jpg",
         categoria: {
             nombre: "Abrigos",
             id: "abrigos"
         },
-        "precio": 120000
+        precio: 120000
     },
     {
         id: "abrigo-04",
-        titulo: "Abrigo Femenino 1 ",
-        imagen: "./imagenes/Mabrigo1.jpg",
+        titulo: "Abrigo Femenino",
+        imagen: "./imagenes/abrigo4.jpg",
         categoria: {
-            "nombre": "Abrigos",
-            "id": "abrigos"
+            nombre: "Abrigos",
+            id: "abrigos"
         },
         precio: 120000
     },
     {
         id: "abrigo-05",
-        titulo: "Abrigo Femenino 2",
-        imagen: "./imagenes/Mabrigo2.jpg",
-        categoria: {
-            nombre: "Abrigos",
-            id: "abrigos"
-        },
-        "precio": 120000
-    },
-    {
-        id: "abrigo-06",
-        titulo: "Abrigo Femenino 3",
-        imagen: "./imagenes/Mabrigo3.jpg",
+        titulo: "Abrigo Femenino",
+        imagen: "./imagenes/abrigo5.jpg",
         categoria: {
             nombre: "Abrigos",
             id: "abrigos"
@@ -60,18 +50,148 @@
         precio: 120000
     },
     {
-        id: "pantalon-01",
-        titulo: "Pantalón Masculino  1",
-        imagen: "./imgenes/pantalon1.jpg",
+        id: "abrigo-06",
+        titulo: "Abrigo Femenino",
+        imagen: "./imagenes/abrigo6.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 120000
+    },
+    {
+        id: "sueter-01",
+        titulo: "Sueter Masculino",
+        imagen: "./imagenes/camiseta1.jpg",
+        categoria: {
+            nombre: "Sueter",
+            id: "sueter"
+        },
+        precio: 100000
+    },
+    {
+        id: "sueter-02",
+        titulo: "Sueter Masculino",
+        imagen: "./imagenes/camiseta2.jpg",
+        categoria: {
+            nombre: "Sueter",
+            id: "sueter"
+        },
+        precio: 100000
+    },
+    {
+        id: "sueter-03",
+        titulo: "Sueter Masculino",
+        imagen: "./imagenes/camiseta3.jpg",
+        categoria: {
+            nombre: "Sueter",
+            id: "sueter"
+        },
+        precio: 100000
+    },
+    {
+        id: "sueter-04",
+        titulo: "Sueter Femenino",
+        imagen: "./imagenes/camiseta4.jpg",
+        categoria: {
+            nombre: "Sueter",
+            id: "sueter"
+        },
+        precio: 100000
+    },
+    {
+        id: "sueter-05",
+        titulo: "Sueter Femenino",
+        imagen: "./imagenes/camiseta5.jpg",
+        categoria: {
+            nombre: "Sueter",
+            id: "sueter"
+        },
+        precio: 100000
+    },
+    {
+        id: "sueter-06",
+        titulo: "Sueter Femenino",
+        imagen: "./imagenes/camiseta6.jpg",
+        categoria: {
+            nombre: "Sueter",
+            id: "sueter"
+        },
+        precio: 100000
+    },
+    {
+        id: "bermuda-01",
+        titulo: "Bermuda Masculina",
+        imagen: "./imagenes/bermuda1.jpg",
+        categoria: {
+            nombre: "Bermuda",
+            id: "bermuda"
+        },
+        precio: 60000
+    },
+    {
+        id: "bermuda-02",
+        titulo: "Bermuda Masculina",
+        imagen: "./imagenes/bermuda2.jpg",
+        categoria: {
+            nombre: "Bermuda",
+            id: "bermuda"
+        },
+        precio: 60000
+    },
+    {
+        id: "bermuda-03",
+        titulo: "Bermuda Masculina",
+        imagen: "./imagenes/bermuda3.jpg",
+        categoria: {
+            nombre: "Bermuda",
+            id: "bermuda"
+        },
+        precio: 60000
+    },
+    {
+        id: "bermuda-04",
+        titulo: "Bermuda Femenina",
+        imagen: "./imagenes/bermuda4.jpg",
+        categoria: {
+            nombre: "Bermuda",
+            id: "bermuda"
+        },
+        precio: 60000
+    },
+    {
+        id: "bermuda-05",
+        titulo: "Bermuda Femenina",
+        imagen: "./imagenes/bermuda5.jpg",
+        categoria: {
+            nombre: "Bermuda",
+            id: "bermuda"
+        },
+        precio: 60000
+    },
+    {
+        id: "bermuda-06",
+        titulo: "Bermuda Femenina",
+        imagen: "./imagenes/bermuda6.jpg",
+        categoria: {
+            nombre: "Bermuda",
+            id: "bermuda"
+        },
+        precio: 60000
+    },
+    {
+        id: "pantalones-01",
+        titulo: "Pantalones Masculinos",
+        imagen: "./imagenes/pantalon1.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
         },
-        "precio": 100000
+        precio: 100000
     },
     {
-        id: "pantalon-02",
-        titulo: "Pantalón Masculino 2 ",
+        id: "pantalones-02",
+        titulo: "Pantalones Masculinos",
         imagen: "./imagenes/pantalon2.jpg",
         categoria: {
             nombre: "Pantalones",
@@ -80,19 +200,9 @@
         precio: 100000
     },
     {
-        id: "pantalon-03",
-        titulo: "Pantalón Masculino 3",
-        imagen: "./img/pantalon3.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        "precio": 100000
-    },
-    {
-        id: "pantalon-04",
-        titulo: "Pantalón Femenino 1",
-        imagen: "./img/Mpantalones1.jpg",
+        id: "pantalones-03",
+        titulo: "Pantalones Masculinos",
+        imagen: "./imagenes/pantalon3.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
@@ -100,9 +210,9 @@
         precio: 100000
     },
     {
-        id: "pantalon-05",
-        titulo: "Pantalón Femenino 2",
-        imagen: "./imagenes/Mpantalones2.jpg",
+        id: "pantalones-04",
+        titulo: "Pantalones Femeninos",
+        imagen: "./imagenes/pantalones4.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
@@ -110,9 +220,9 @@
         precio: 100000
     },
     {
-        id: "pantalon-06",
-        titulo: "Pantalón Femenino 3",
-        imagen: "./imagenes/Mpantalones3.jpg",
+        id: "pantalones-05",
+        titulo: "Pantalones Femeninos",
+        imagen: "./imagenes/pantalones5.jpg",
         categoria: {
             nombre: "Pantalones",
             id: "pantalones"
@@ -120,126 +230,16 @@
         precio: 100000
     },
     {
-       id: "bermuda-01",
-        titulo: "Bermuda Masculina 1",
-        imagen: "./imagenes/Bermudah1.jpg",
+        id: "pantalones-06",
+        titulo: "Pantalones Femeninos",
+        imagen: "./imagenes/pantalones6.jpg",
         categoria: {
-            nombre: "Bermudas",
-            id: "bermudas"
+            nombre: "Pantalones",
+            id: "pantalones"
         },
         precio: 100000
     },
-    {
-        id: "bermuda-02",
-        titulo: "Bermuda Masculina 2",
-        imagen: "./imagenes/bermudah2.jpg",
-        categoria: {
-            nombre: "Bermudas",
-            id: "bermudas"
-        },
-        precio: 100000
-    },
-    {
-        id: "bermuda-03",
-        titulo: "Bermuda Masculina 3",
-        imagen: "./imagenes/bermudah3.jpg",
-        categoria: {
-            nombre: "Bermudas",
-            id: "bermudas"
-        },
-        precio: 100000
-    },
-    {
-        id: "bermuda-04",
-        titulo: "Bermuda Femenina 1",
-        imagen: "./imagenes/BermudaM1.jpg",
-        categoria: {
-            nombre: "Bermudas",
-            id: "bermudas"
-        },
-        precio: 100000
-    },
-    {
-        id: "bermuda-05",
-    titulo: "Bermuda Femenina 2",
-        imagen: "./imagenes/bermudaM2.jpg",
-        categoria: {
-            nombre: "Bermudas",
-            id: "bermudas"
-        },
-        precio: 100000
-    },
-    {
-        id: "bermuda-06",
-        titulo: "Bermuda Femenina 3",
-        imagen: "./imagenes/bermudaM3.jpg",
-        categoria: {
-            nombre: "Bermudas",
-            id: "bermudas"
-        },
-        precio: 100000
-    },
-    {
-        id:"camiseta-01",
-        titulo:"Camiseta Masculina 1",
-        imagen:"./imagenes/camiseta1.jpg",
-        categoria: {
-            nombre:"Camiseta",
-            id: "camiseta"
-        },
-        precio: 90000
-    },
-    {
-        id: "camiseta-02",
-        titulo: "Camiseta Masculina 2",
-        imagen: "./imagenes/camiseta2.jpg",
-        categoria: {
-            nombre: "Camiseta",
-            id: "camiseta"
-        },
-        precio: 90000
-    },
-    {
-        id:"camiseta-03",
-        titulo: "Camiseta Masculina 3",
-        imagen:"./imagenes/camiseta3.jpg",
-        categoria: {
-            nombre: "Camiseta",
-            id: "camiseta"
-        },
-        precio: 90000
-    },
-    {
-        id: "camiseta-04",
-        titulo: "Camiseta Femenina 4",
-        imagen: "./imagenes/camiseta4.jpg",
-        categoria: {
-            nombre: "Camiseta",
-            id: "camiseta"
-        },
-        precio: 90000
-    },
-    {
-        id: "camiseta-05",
-        titulo: "Camiseta Femenina 5",
-        imagen: "./imagenes/camiseta5.jpg",
-        categoria: {
-            nombre: "Camiseta",
-            id: "camiseta"
-        },
-        precio: 90000
-    },
-    {
-        id: "camiseta-06",
-        titulo: "Camiseta Femenina 6",
-        imagen: "./imagenes/camiseta6.jpg",
-        categoria: {
-            nombre: "Camiseta",
-            id: "camiseta"
-        },
-        precio: 90000
-    },
-];
+ ]
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 
